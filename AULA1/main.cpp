d6a6501106baac7af3ffee8bc55fb64e163d2db4
Bernardo Viseu <bernardo2viseu@gmail.com>
2019-02-15 20:26:27 +0000

    fixes tabs

--- AULA1/main.cpp
+++ AULA1/main.cpp
@@ -11,41 +11,41 @@ double size = 1;
 
 void changeSize(int w, int h) {
 
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window with zero width).
-	if(h == 0)
-		h = 1;
+    // Prevent a divide by zero, when window is too short
+    // (you cant make a window with zero width).
+    if(h == 0)
+        h = 1;
 
-	// compute window's aspect ratio 
-	float ratio = w * 1.0 / h;
+    // compute window's aspect ratio 
+    float ratio = w * 1.0 / h;
 
-	// Set the projection matrix as current
-	glMatrixMode(GL_PROJECTION);
-	// Load Identity Matrix
-	glLoadIdentity();
-	
-	// Set the viewport to be the entire window
+    // Set the projection matrix as current
+    glMatrixMode(GL_PROJECTION);
+    // Load Identity Matrix
+    glLoadIdentity();
+    
+    // Set the viewport to be the entire window
     glViewport(0, 0, w, h);
 
-	// Set perspective
-	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+    // Set perspective
+    gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
 
-	// return to the model view matrix mode
-	glMatrixMode(GL_MODELVIEW);
+    // return to the model view matrix mode
+    glMatrixMode(GL_MODELVIEW);
 }
 
 
 
 void renderScene(void) {
 
-	// clear buffers
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // clear buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// set the camera
-	glLoadIdentity();
-	gluLookAt(0.0,0.0,5.0, 
-		      0.0,0.0,-1.0,
-			  0.0f,1.0f,0.0f);
+    // set the camera
+    glLoadIdentity();
+    gluLookAt(0.0,0.0,5.0, 
+              0.0,0.0,-1.0,
+              0.0f,1.0f,0.0f);
 
     // put drawing instructions here
     double sinSize = sin(size);
@@ -54,8 +54,8 @@ void renderScene(void) {
 
 
 
-	// End of frame
-	glutSwapBuffers();
+    // End of frame
+    glutSwapBuffers();
 }
 
 
@@ -78,13 +78,13 @@ int main(int argc, char **argv) {
 
 
     // OpenGL settings
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	glClearColor(0.0f,0.0f,0.0f,0.0f);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glClearColor(0.0f,0.0f,0.0f,0.0f);
 
     // enter GLUT's main loop
-	glutMainLoop();
-	
-	return 1;
+    glutMainLoop();
+    
+    return 1;
 }
 
